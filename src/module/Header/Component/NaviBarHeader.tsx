b17f40339922f7b2b0d38d0d1beda3804db08156
polar_foxx <suchovsky.michal@gmail.com>
2023-04-03 17:36:34 +0200

    headerButtonResponse

--- src/module/Header/Component/NaviBarHeader.tsx
+++ src/module/Header/Component/NaviBarHeader.tsx
@@ -2,20 +2,36 @@
 
 function NaviBarHeader(): JSX.Element {
 
-    
+    const handleButoonClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+        const buttonIdent = e.currentTarget.innerText as string
+            console.log(buttonIdent);
+    }
+
     return (
         <div className="naviBlockNavContent">
             <div className="btnBlock">
-                <button>O mne</button>
+                <button
+                    onClick={(e) => handleButoonClick(e)}>
+                    O mne
+                </button>
             </div>
             <div className="btnBlock">
-                <button>Sluzby</button>
+                <button
+                    onClick={(e) => handleButoonClick(e)}>
+                    Sluzby
+                </button>
             </div>
             <div className="btnBlock">
-                <button>Portfolio</button>
+                <button
+                    onClick={(e) => handleButoonClick(e)}>
+                    Portfolio
+                </button>
             </div>
-            <div className="btnBlock"> 
-                <button>Kontakt</button>
+            <div className="btnBlock">
+                <button
+                    onClick={(e) => handleButoonClick(e)}>
+                    Kontakt
+                </button>
             </div>
         </div>
     )
