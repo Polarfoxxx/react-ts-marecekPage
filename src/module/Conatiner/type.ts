c17cabc26f8faec8cff9b90a866b1d13aab36663
polar_foxx <suchovsky.michal@gmail.com>
2023-04-06 16:50:26 +0200

    bug

--- src/module/Conatiner/type.ts
+++ src/module/Conatiner/type.ts
@@ -1,5 +1,11 @@
+import { Dispatch, SetStateAction } from "react"
 
 
 export type Props = {
     children: JSX.Element | JSX.Element[]
+}
+
+export type typeContext = {
+    navButtonName: string,
+    setNavButtonName: Dispatch<SetStateAction<string>> 
 }
