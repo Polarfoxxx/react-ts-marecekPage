b9bb7d03cfb55a256746c232ad31cb80038c62d9
polar_foxx <suchovsky.michal@gmail.com>
2023-04-08 17:24:07 +0200

    create_button_move_top_page

--- src/module/Conatiner/Component/Container.tsx
+++ src/module/Conatiner/Component/Container.tsx
@@ -1,8 +1,12 @@
 import { Props } from "../type";
 import { typeContext } from "../type";
 
-import { createContext, useState } from "react";
+import { createContext, useState,} from "react";
 import "../style/container.style.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+
 
 const Context = createContext<typeContext>({
     navButtonName: "",
@@ -10,22 +14,43 @@ const Context = createContext<typeContext>({
 });
 
 const Provider: React.FC<Props> = ({ children }) => {
-    const [navButtonName, setNavButtonName] = useState("");
+    const [navButtonName, setNavButtonName] = useState("")
+    const [dd, setdd] = useState(false)
 
-    /* button na presun na top stranky */
-    const handleScroolTop = () => {
+    /* event na presun na top stranky */
+    const handleScroolTop = (): void => {
         setNavButtonName("default")
     }
+    /* scroll event na zobrazenie Btn */
+    const handleScrollPage = (eScroll: React.UIEvent<HTMLElement>): void => {
+        if (eScroll.currentTarget.scrollTop > 300) {
+            setTimeout(() => {
+                setdd(true)
+                setNavButtonName("")
+            }, 500)
+
+        } else if (eScroll.currentTarget.scrollTop < 300) {
+            setTimeout(() => {
+                setdd(false)
+                setNavButtonName("")
+            }, 500)
+        }
+    }
+
 
     return (
-        <div className="container">
-            <span className="scroller"
+        <div className="container"
+            onScroll={handleScrollPage}>
+            <span style={{ opacity: dd ? "1" : "0", zIndex: dd ? "5" : "-5"  }}
+                className="scroller"
                 onClick={handleScroolTop}>
-                to top
+                {<FontAwesomeIcon icon={faArrowUp} size="lg" />}
             </span>
-            <Context.Provider value={{ navButtonName, setNavButtonName }}>
-                {children}
-            </Context.Provider>
+            <div className="container-Content">
+                <Context.Provider value={{ navButtonName, setNavButtonName }}>
+                    {children}
+                </Context.Provider>
+            </div>
         </div>
     );
 };
