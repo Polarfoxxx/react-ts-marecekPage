c69d1132c33d6e2eb55142df8394db079f32ffd8
polar_foxx <suchovsky.michal@gmail.com>
2023-04-09 11:57:18 +0200

    services_dizajn

--- src/module/ServicesBlock/Component/ProductServices.tsx
+++ src/module/ServicesBlock/Component/ProductServices.tsx
@@ -1,9 +1,44 @@
 import "../style/productServices.style.css"
 
 function ProductServices(): JSX.Element {
-    return(
-        <div>
-            
+    return (
+        <div className="productServContent">
+            <div className="productServOne ContentRow">
+                <div className="productBlock">
+                    <div className="product">
+                        <div className="prodTittle">
+                            <span>01</span>
+                            <span>Logo dizajn</span>
+                        </div>
+                        <div className="prodContent">
+                            <span>
+                                Quam nulla porttitor massa id.
+                                Consectetur purus ut faucibus pulvinar
+                                elementum integer enim neque volutpat.
+                                Quam nulla porttitor massa id.
+                                Quam nulla porttitor massa id.
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="productBlock">
+                    <div className="product">
+
+                    </div>
+                </div>
+            </div>
+            <div className="productServTwo ContentRow">
+                <div className="productBlock">
+                    <div className="product">
+
+                    </div>
+                </div>
+                <div className="productBlock">
+                    <div className="product">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
